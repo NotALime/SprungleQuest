2f05a9da787ddd0e9ce79485128542b66d1a8295
Lemon Gamez <64447652+NotALime@users.noreply.github.com>
2024-06-13 15:10:14 -0700

    started adding main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static void LoadScene(string scene)
+    { 
+        SceneManager.LoadScene(scene);
+    }
+    public static void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void OpenMenu(GameObject menu)
+    {
+        menu.SetActive(!menu.activeInHierarchy);
+    }
+}
